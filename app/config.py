22fe56064ff8630d1d9c37f72d5b7327255a0bab
likhi <164597427+Likhi0511@users.noreply.github.com>
2026-04-01 21:19:46 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv(override=True)
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+INDEX_NAME = os.getenv("INDEX_NAME", "rag-project-index")
+
+DATABASE_URL = "postgresql+psycopg2://postgres:likhi1105@localhost:5432/rag_db"
